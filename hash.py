5bac46f70ee8d313a77cdacbe9e3fdb691646e09
maximilianoangel <maximiliano.angel@mail.udp.cl>
2021-10-06 00:07:04 -0600

    Create hash.py
    hash_v1

---
+++ hash.py
@@ -0,0 +1,79 @@
+import fileinput
+def rellenar(palabra,pos):
+    salt=["A","b","C","d","E"]
+    if pos == len(salt):
+        pos=0
+    if len(palabra) <32:
+        pal=palabra+salt[pos]
+        pos=pos+1
+        pal=rellenar(pal,pos)
+        return pal
+    else:
+        return palabra
+
+def hash(palabra,pos,Spos,aux):
+    salt=["Z","W","R","Q","Y"]
+    if Spos == len(salt):
+        Spos=0
+    if pos<len(palabra):
+        A=int(ord(palabra[pos]))
+        B=int(ord(salt[Spos]))
+        C=len(palabra)
+        aux.append(chr((B%A)+C))
+        pos=pos+1
+        Spos=Spos+1
+        pal=hash(palabra,pos,Spos,aux)
+        return pal
+    else:
+        return aux
+
+def reducir(palabra,PosF,PosI,aux):
+    if len(aux)==32:
+        return aux
+    else:
+        A=ord(palabra[PosF])
+        B=ord(palabra[PosI])
+        posI=PosI+1
+        posF=PosF-1
+        aux.append(chr(A+B))
+        pal=reducir(palabra,posF,posI,aux)
+        return pal
+
+
+
+
+def Iniciar(palabra):
+    aux=[]
+    aux2=''
+    TamañoOriginal=len(palabra)
+    if TamañoOriginal> 32:
+        red=reducir(palabra,TamañoOriginal-1,0,aux)
+        for v in red:
+            aux2=aux2+v
+        aux=[]
+        f=hash(aux2,0,0,aux)
+        aux2=''
+        for v in f:
+            aux2=aux2+v
+        return aux2
+    elif TamañoOriginal< 32:
+        pal=rellenar(palabra,0)
+        val=hash(pal,0,0,aux)
+        for v in val:
+            aux2=aux2+v
+        return aux2
+    else:
+        return hash(palabra,0,0,aux)
+
+
+if __name__ == '__main__':
+    opcion = int(input("Seleccione opcion:\n  1. Ingresar un dato unico\n  2. Ingrear archivos de datos\n "))
+    if opcion==1:
+        preguntar=input("Ingrese String a Hashear:\n")
+        print(Iniciar(preguntar))
+    elif opcion==2:
+        data=[]
+        for lines in fileinput.input():
+            data.append(lines.rstrip())
+        for palabra in data:
+            print("El Hash es: " + Iniciar(palabra) + "\n")
